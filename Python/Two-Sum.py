a8bfd90d66d9669dd01ee1a5fc2d1a7195441067
s-top <15634647@qq.com>
2018-08-18 23:49:16 +0800

    Two-Sum

---
+++ Python/Two-Sum.py
@@ -0,0 +1,17 @@
+# Example:
+# Given nums = [2, 7, 11, 15], target = 9,
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1].
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i, num in enumerate(nums):
+            pair = target - num
+            if pair in nums[i + 1:]:
+                return [i, nums.index(pair, i + 1)]
+        return None
